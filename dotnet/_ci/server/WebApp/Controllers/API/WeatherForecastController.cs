882c36552efcb50b353a14fae93eb2c79101192b
valorad <valorad@outlook.com>
2020-09-02 18:25:44 -0400

    webapp docker file + read me

--- dotnet/_ci/server/WebApp/Controllers/API/WeatherForecastController.cs
+++ dotnet/_ci/server/WebApp/Controllers/API/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using WebApp.Models;
 
 namespace WebApp.Controllers.API
 {
--- dotnet/_ci/server/WebApp/Models/WeatherForecast.cs
+++ dotnet/_ci/server/WebApp/Models/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace WebApp
+namespace WebApp.Models
 {
     public class WeatherForecast
     {
