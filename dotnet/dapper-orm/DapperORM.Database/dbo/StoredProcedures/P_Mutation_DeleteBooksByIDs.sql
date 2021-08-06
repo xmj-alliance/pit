f48b394961393224e69e25423139d346cc64e7f9
valorad <valorad@outlook.com>
2021-08-06 15:12:49 -0400

    Book Sps for crud

---
+++ dotnet/dapper-orm/DapperORM.Database/dbo/StoredProcedures/P_Mutation_DeleteBooksByIDs.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[P_Mutation_DeleteBooksByIDs]
+	@ids dbo.[IntList] READONLY
+AS
+BEGIN TRANSACTION
+
+	UPDATE Books
+
+	SET [DeleteDate] = GETDATE()
+
+	FROM Books
+
+	WHERE Id IN (SELECT Id FROM @ids)
+
+COMMIT
---
+++ dotnet/dapper-orm/DapperORM.Database/dbo/StoredProcedures/P_Mutation_SaveBooks.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE [dbo].[P_Mutation_SaveBooks]
+	@inputBooks dbo.[InputBooks] READONLY
+AS
+BEGIN TRANSACTION
+
+	-- default values
+	DECLARE @defaultRating FLOAT = 0.0
+	DECLARE @defaultUpdateDate DATETIME = GETDATE()
+
+	-- select books without id, then do insert
+	INSERT INTO Books
+	SELECT 
+		[DBName],
+		[Title],
+		ISNULL([Rating], @defaultRating),
+		ISNULL([UpdateDate], @defaultUpdateDate),
+		[DeleteDate]
+	FROM @inputBooks
+	WHERE Id is null
+
+	-- select books with id, then do replace (update)
+	UPDATE Books
+
+	SET [DBName] = inputBooks.[DBName],
+		[Title] = inputBooks.[Title],
+		[Rating] = ISNULL(inputBooks.[Rating], @defaultRating),
+		[UpdateDate] = ISNULL(inputBooks.[UpdateDate], @defaultUpdateDate),
+		[DeleteDate] = inputBooks.[DeleteDate]
+
+	FROM Books targetBooks
+		INNER JOIN @inputBooks inputBooks ON inputBooks.Id = targetBooks.Id
+
+COMMIT
