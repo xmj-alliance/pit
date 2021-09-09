471726ef94f2e1c3142dc84cacc9adef75ac41c7
valorad <valorad@outlook.com>
2021-09-09 15:44:00 -0400

    basic layouts for two paper views

--- frontend/react/exam-paper-data-passing/src/views/paper/paper.view.tsx
+++ frontend/react/exam-paper-data-passing/src/views/paper/paper.view.tsx
@@ -1,5 +1,48 @@
+import paperStyles from "src/views/paper.module.css";
+import styles from "./paper.view.module.css";
+
 const PaperView = (): JSX.Element => (
-  <section>PaperView works</section>
+  <section className={styles.paperView}>
+    <header>
+      <h1 className={paperStyles.paperTitle}>Super hard exam</h1>
+      <p className={paperStyles.scoreHolder}>
+        <small>Score:</small>
+        <span>100</span>
+      </p>
+    </header>
+    <main className={paperStyles.paperContent}>
+      <h2 className={paperStyles.questionTitle}>
+        question: blah blah blah blah blah blah blah blah blah
+        blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah
+        blah blah blah blah blah blah blah blah
+      </h2>
+      <div>
+        <form className={paperStyles.choices} action="none">
+          <label className={paperStyles.optionCaption}>
+            <input type="radio" name="answer1" />
+            (A) lalala
+          </label>
+          <label className={paperStyles.optionCaption}>
+            <input type="radio" name="answer1" />
+            (B) lalala
+          </label>
+          <label className={paperStyles.optionCaption}>
+            <input type="radio" name="answer1" />
+            (C) lalala
+          </label>
+          <label className={paperStyles.optionCaption}>
+            <input type="radio" name="answer1" />
+            (D) lalala
+          </label>
+        </form>
+      </div>
+
+    </main>
+    <footer className={paperStyles.bottomControls}>
+      <button type="button"> Submit </button>
+    </footer>
+  </section>
 );
 
 export default PaperView;
--- frontend/react/exam-paper-data-passing/src/views/paperStore/paperStore.view.tsx
+++ frontend/react/exam-paper-data-passing/src/views/paperStore/paperStore.view.tsx
@@ -1,5 +1,43 @@
+import paperStyles from "src/views/paper.module.css";
+import styles from "./paperStore.view.module.css";
+
 const PaperStoreView = (): JSX.Element => (
-  <section>PaperStoreView works</section>
+  <section className={styles.paperStoreView}>
+    <header>
+      <h1 className={paperStyles.paperTitle}>Super hard exam (with Redux Store)</h1>
+      <p className={paperStyles.scoreHolder}>
+        <small>Score:</small>
+        <span>100</span>
+      </p>
+    </header>
+    <main className={paperStyles.paperContent}>
+      <h2 className={paperStyles.questionTitle}> question: blah</h2>
+      <div>
+        <form className={paperStyles.choices} action="none">
+          <label className={paperStyles.optionCaption}>
+            <input type="radio" name="answer2" />
+            (A) lalala
+          </label>
+          <label className={paperStyles.optionCaption}>
+            <input type="radio" name="answer2" />
+            (B) lalala
+          </label>
+          <label className={paperStyles.optionCaption}>
+            <input type="radio" name="answer2" />
+            (C) lalala
+          </label>
+          <label className={paperStyles.optionCaption}>
+            <input type="radio" name="answer2" />
+            (D) lalala
+          </label>
+        </form>
+      </div>
+
+    </main>
+    <footer className={paperStyles.bottomControls}>
+      <button type="button"> Submit </button>
+    </footer>
+  </section>
 );
 
 export default PaperStoreView;
