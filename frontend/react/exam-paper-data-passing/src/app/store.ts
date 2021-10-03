a1ab88ac84a9f7747e73a34b2209199500b67ee9
valorad <valorad@outlook.com>
2021-10-03 11:31:21 -0400

    redux store example of exam paper

--- frontend/react/exam-paper-data-passing/src/app/store.ts
+++ frontend/react/exam-paper-data-passing/src/app/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import paperReducer from 'src/features/paper/paperSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    paper: paperReducer,
   },
 });
 
--- frontend/react/exam-paper-data-passing/src/views/paperStore/paperStore.view.tsx
+++ frontend/react/exam-paper-data-passing/src/views/paperStore/paperStore.view.tsx
@@ -1,5 +1,10 @@
-import { useState } from "react";
-import QuestionList from "src/components/question/questionList";
+/* eslint-disable no-restricted-syntax */
+import { useAppDispatch, useAppSelector } from "src/app/hooks";
+import QuestionListStore from "src/components/question/questionListStore";
+import { selectScore, setIsUserAnswerSubmitted, setRightAnswerStore } from "src/features/paper/paperSlice";
+import { IChoice } from "src/models/choice";
+import { IAnswerStore } from "src/models/paper";
+import { IQuestion } from "src/models/question";
 import paperStyles from "src/views/paper.module.css";
 import styles from "./paperStore.view.module.css";
 
@@ -32,8 +37,37 @@ const data = {
   ],
 };
 
+const questionsWithRightChoices: Partial<IQuestion>[] = [
+  {
+    id: "d2f4af45-5c5f-4de3-8e3c-bfbb4294aed1",
+    rightChoices: [
+      {
+        id: "1abbc251-9489-4ee7-b1ad-1f8a045d8b1c",
+        value: "(D) Response",
+      },
+    ],
+  },
+];
+
+const createRightAnswerStore = (): IAnswerStore => {
+  const rightAnswerStore = {} as IAnswerStore;
+
+  for (const question of questionsWithRightChoices) {
+    rightAnswerStore[question.id || "missingID"] = question.rightChoices as IChoice[];
+  }
+
+  return rightAnswerStore;
+};
+
 const PaperStoreView = (): JSX.Element => {
-  const [score, setScore] = useState(0);
+  const dispatch = useAppDispatch();
+  const score = useAppSelector(selectScore);
+
+  const onSubmitButtonClick = (e: React.MouseEvent): void => {
+    const nextRightAnswerStore = createRightAnswerStore();
+    dispatch(setIsUserAnswerSubmitted(true));
+    dispatch(setRightAnswerStore(nextRightAnswerStore));
+  };
 
   return (
     <section className={styles.paperStoreView}>
@@ -45,10 +79,10 @@ const PaperStoreView = (): JSX.Element => {
         </p>
       </header>
       <main className={paperStyles.paperContent}>
-        <QuestionList data={data} />
+        <QuestionListStore data={data} />
       </main>
       <footer className={paperStyles.bottomControls}>
-        <button type="button"> Submit </button>
+        <button type="button" onClick={onSubmitButtonClick}> Submit </button>
       </footer>
     </section>
   );
