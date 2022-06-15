1dd424eb29c7865a73d161a39865c0e4ed815cae
Crucsoul Dragonborn <longyiszh@gmail.com>
2022-06-15 14:09:25 -0400

    phone pk: add identitymodel and graphql client

--- dotnet/middle-stage-phone-pk/MiddleStagePhonePK.App/Controllers/APIController.cs
+++ dotnet/middle-stage-phone-pk/MiddleStagePhonePK.App/Controllers/APIController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using GraphQL;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.SystemTextJson;
+using Microsoft.AspNetCore.Mvc;
 using MiddleStagePhonePK.App.Models;
 
 namespace MiddleStagePhonePK.App.Controllers;
@@ -7,6 +12,13 @@ namespace MiddleStagePhonePK.App.Controllers;
 [ApiController]
 public class APIController : ControllerBase
 {
+    private readonly HttpClient client;
+
+    public APIController(IHttpClientFactory factory)
+    {
+        client = factory.CreateClient("client");
+    }
+
     [HttpGet]
     public CommonMessage GetReadyState()
     {
@@ -15,4 +27,71 @@ public class APIController : ControllerBase
             Content: "API Works!"
         );
     }
+
+    [HttpGet]
+    [Route("/phones")]
+    public async Task<GraphQLResponse<ResponseType>> GetPhones()
+    {
+        var graphQLClient = new GraphQLHttpClient(
+            new GraphQLHttpClientOptions
+            {
+                EndPoint = new Uri($"{client.BaseAddress}/graphql"),
+                UseWebSocketForQueriesAndMutations = false
+            },
+            new SystemTextJsonSerializer(
+                new JsonSerializerOptions()
+                {
+                    Converters = {
+                        new JsonStringEnumConverter(new ConstantCaseJsonNamingPolicy(), false)
+                    }
+                }.SetupImmutableConverter()
+            ),
+            client
+        );
+
+        var gqlRequest = new GraphQLRequest {
+            Query = $@"
+            {{
+                findPhoneContent(id: ""46f3ee81-925e-48ce-a07e-c05e2398035c"") {{
+		            id
+		            data {{
+			            name {{
+				            en
+			            }}
+			            description {{
+				            en
+			            }}
+		            }}
+	            }}
+            }}
+            "
+        };
+
+        var graphQLResponse = await graphQLClient.SendQueryAsync<ResponseType>(gqlRequest);
+
+        return graphQLResponse;
+    }
+}
+
+public class ResponseType
+{
+    public FindPhoneContentType? FindPhoneContent { get; set; }
+}
+
+
+public class FindPhoneContentType
+{
+    public string? Id { get; set; }
+    public PhoneDataType? Data { get; set; }
+}
+
+public class PhoneDataType
+{
+    public SquidexI18NType? Name { get; set; }
+    public SquidexI18NType? Description { get; set; }
+}
+
+public class SquidexI18NType
+{
+    public string? En { get; set; }
 }
--- dotnet/middle-stage-phone-pk/MiddleStagePhonePK.App/Program.cs
+++ dotnet/middle-stage-phone-pk/MiddleStagePhonePK.App/Program.cs
@@ -1,3 +1,5 @@
+﻿using IdentityModel.Client;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,38 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAccessTokenManagement( options =>
+    {
+
+        string backendAuthAddress = builder.Configuration.GetValue<string>("backendAuthURL")
+                                    ?? throw new ApplicationException("backendAuthURL cannot be null");
+        string backendAuthClientID = builder.Configuration.GetValue<string>("backendAuthClientID")
+                                    ?? throw new ApplicationException("backendAuthClientID cannot be null");
+        string backendAuthClientSecret = builder.Configuration.GetValue<string>("backendAuthClientSecret")
+                                    ?? throw new ApplicationException("backendAuthClientSecret cannot be null");
+        string backendAuthClientScope = builder.Configuration.GetValue<string>("backendAuthClientScope")
+                                    ?? throw new ApplicationException("backendAuthClientScope cannot be null");
+
+        string serverName = (new Uri(backendAuthAddress)).Host;
+
+        options.Client.Clients.Add(serverName, new ClientCredentialsTokenRequest 
+        {
+            Address = backendAuthAddress,
+            ClientId = backendAuthClientID,
+            ClientSecret = backendAuthClientSecret,
+            Scope = backendAuthClientScope
+        });
+    }
+);
+
+builder.Services.AddClientAccessTokenHttpClient("client", configureClient: client =>
+{
+    string backendBaseURL = builder.Configuration.GetValue<string>("backendBaseURL")
+                                ?? throw new ApplicationException("backendBaseURL cannot be null");
+
+    client.BaseAddress = new Uri(backendBaseURL);
+});
 
 var app = builder.Build();
 
