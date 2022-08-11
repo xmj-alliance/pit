712d6c6ef2f47fdcd3f590ae110767c5a3bc100a
Crucsoul Dragonborn <longyiszh@gmail.com>
2022-08-11 14:01:51 -0400

    phonepk: unit test layout

---
+++ dotnet/middle-stage-phone-pk/MiddleStagePhonePK.Test/CheerUp/CheerUpTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MiddleStagePhonePK.App.Services;
+using MiddleStagePhonePK.Test.Infrastructure;
+
+namespace MiddleStagePhonePK.Test.CheerUp;
+
+[Collection("Sequential")]
+public class CheerUpTest : IClassFixture<ServiceFixture>
+{
+    private readonly IHost testHost;
+    private readonly IDataAccessService dataAccessService;
+
+    public CheerUpTest(ServiceFixture fixture)
+    {
+        testHost = fixture.TestHost;
+        dataAccessService = testHost.Services.GetService<IDataAccessService>()!;
+    }
+
+    [Fact]
+    public void HaveFun()
+    {
+        Assert.Equal("😙", "😙");
+        Assert.NotNull(dataAccessService);
+    }
+
+}
---
+++ dotnet/middle-stage-phone-pk/MiddleStagePhonePK.Test/Infrastructure/Services/MockDataAccessService.cs
@@ -0,0 +1,26 @@
+﻿using MiddleStagePhonePK.App.Models.Squidex;
+using MiddleStagePhonePK.App.Services;
+
+namespace MiddleStagePhonePK.Test.Infrastructure.Services;
+public class MockDataAccessService : IDataAccessService
+{
+    public async Task<IEnumerable<SquidexMutationTypes>> CreateContents<T>(string gqlMutationName, string gqlInputTypeName, IEnumerable<T> newItems, string gqlResultSelector)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IDictionary<string, SquidexMutationTypes>> DeleteContents(string gqlMutationName, IEnumerable<string> ids, string gqlResultSelector)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<SquidexQueryTypes> QueryContentsByIDs(string gqlQueryName, IEnumerable<string> ids, string gqlResultSelector)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<SquidexMutationTypes>> UpdateContents<T>(string gqlMutationName, string gqlInputTypeName, IDictionary<string, T> idNewItemMap, string gqlResultSelector)
+    {
+        throw new NotImplementedException();
+    }
+}
