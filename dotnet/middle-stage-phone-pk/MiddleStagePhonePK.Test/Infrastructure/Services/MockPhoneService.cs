e6ac7e7e4c860e976d9e60478d32cc649034518d
Crucsoul Dragonborn <longyiszh@gmail.com>
2022-08-14 19:05:34 -0400

    phonePK: add/update/delete phone tests

--- dotnet/middle-stage-phone-pk/MiddleStagePhonePK.Test/Infrastructure/Services/MockPhoneService.cs
+++ dotnet/middle-stage-phone-pk/MiddleStagePhonePK.Test/Infrastructure/Services/MockPhoneService.cs
@@ -6,6 +6,8 @@ using Moq;
 namespace MiddleStagePhonePK.Test.Infrastructure.Services;
 public class MockPhoneService : PhoneService
 {
+    private static MockPhoneService _instance;
+
     public MockPhoneService(): base(SetupMock())
     { }
 
@@ -13,6 +15,26 @@ public class MockPhoneService : PhoneService
     {
         Mock<IDataAccessService> mock = new();
 
+        Build()
+            .SetupCreateContents(mock)
+            .SetupQueryContentsByIDs(mock)
+            .SetupUpdateContents(mock)
+            .SetupDeleteContents(mock);
+
+        return mock.Object;
+    }
+
+    private static MockPhoneService Build()
+    {
+        if (_instance is null)
+        {
+            _instance = new MockPhoneService();
+        }
+        return _instance;
+    }
+
+    private MockPhoneService SetupQueryContentsByIDs(Mock<IDataAccessService> mock)
+    {
         mock.Setup(service =>
             service.QueryContentsByIDs(
                 It.IsAny<string>(),
@@ -39,6 +61,101 @@ public class MockPhoneService : PhoneService
             )
         ));
 
-        return mock.Object;
+        return _instance;
     }
+
+    private MockPhoneService SetupCreateContents(Mock<IDataAccessService> mock)
+    {
+        mock.Setup(service =>
+            service.CreateContents(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<List<SquidexPhoneDataInputDto>>(),
+                It.IsAny<string>()
+            )
+        ).Returns(Task.FromResult(
+            new List<SquidexMutationTypes>()
+            {
+                new SquidexMutationTypes()
+                {
+                    CreatePhoneContent = new PhoneQueryContentType()
+                    {
+                        Id = "someguid",
+                        Created = DateTime.Now,
+                        CreatedBy = "me",
+                        Data = new PhoneGraphDataType(
+                            Name: new SquidexI18NDto(
+                                En: "Testing iPad"
+                            ),
+                            Description: new SquidexI18NDto(
+                                En: "Testing iPad"
+                            )
+                        )
+                    }
+                }
+            } as IEnumerable<SquidexMutationTypes>
+        ));
+
+        return _instance;
+    }
+
+    private MockPhoneService SetupUpdateContents(Mock<IDataAccessService> mock)
+    {
+        mock.Setup(service =>
+            service.UpdateContents(
+                It.IsAny<string>(),
+                It.IsAny<string>(),
+                It.IsAny<IDictionary<string, SquidexPhoneDataInputDto>>(),
+                It.IsAny<string>()
+            )
+        ).Returns(Task.FromResult(
+            new List<SquidexMutationTypes>()
+            {
+                new SquidexMutationTypes()
+                {
+                    UpdatePhoneContent = new PhoneQueryContentType()
+                    {
+                        Id = "someguid",
+                        LastModified = DateTime.Now,
+                        LastModifiedBy = "me",
+                        Data = new PhoneGraphDataType(
+                            Name: new SquidexI18NDto(
+                                En: "updated iPad nimi 4"
+                            ),
+                            Description: new SquidexI18NDto(
+                                En: "updated iPad nimi 4"
+                            )
+                        )
+                    }
+                }
+            } as IEnumerable<SquidexMutationTypes>
+        ));
+
+        return _instance;
+    }
+
+    private MockPhoneService SetupDeleteContents(Mock<IDataAccessService> mock)
+    {
+        mock.Setup(service =>
+            service.DeleteContents(
+                It.IsAny<string>(),
+                It.IsAny<List<string>>(),
+                It.IsAny<string>()
+            )
+        ).Returns(Task.FromResult(
+            new Dictionary<string, SquidexMutationTypes>()
+            {
+                ["someguid"] = new SquidexMutationTypes()
+                {
+                    DeletePhoneContent = new SquidexEntitySavedResultDto(
+                        Version: 1
+                    )
+                }
+            } as IDictionary<string, SquidexMutationTypes>
+        ));
+
+        return _instance;
+    }
+
+
 }
--- dotnet/middle-stage-phone-pk/MiddleStagePhonePK.Test/PhoneTests/PhoneTest.cs
+++ dotnet/middle-stage-phone-pk/MiddleStagePhonePK.Test/PhoneTests/PhoneTest.cs
@@ -22,15 +22,96 @@ public class PhoneTest : IClassFixture<ServiceFixture>
     {
         List<Phone> phones = await phoneService.GetByIDs(ids);
 
-        Assert.Single(phones);
+        Assert.Equal(ids.Count, phones.Count);
 
         Assert.Equal(ids.First(), phones.First().ID);
     }
+
+    [Theory]
+    [ClassData(typeof(PhoneCreateData))]
+    public async Task GivePhones_ReturnAddedPhones(List<InputPhone> newPhones)
+    {
+        List<Phone> addedPhones = await phoneService.Add(newPhones);
+
+        Assert.Equal(newPhones.Count, addedPhones.Count);
+
+        Assert.Equal(newPhones.First().Name, addedPhones.First().Name);
+    }
+
+    [Theory]
+    [ClassData(typeof(PhoneUpdateData))]
+    public async Task GivePhones_ReturnUpdatedPhones(List<InputPhone> updatingPhones)
+    {
+        List<Phone> updatedPhones = await phoneService.Update(updatingPhones);
+
+        Assert.Equal(updatingPhones.Count, updatedPhones.Count);
+
+        Assert.Equal(updatingPhones.First().Name, updatedPhones.First().Name);
+    }
+
+    [Theory]
+    [ClassData(typeof(PhoneDeleteData))]
+    public async Task GiveIDs_ReturnDeletedPhones(List<string> ids)
+    {
+        List<Phone> deletedPhones = await phoneService.Delete(ids);
+
+        Assert.Equal(ids.Count, deletedPhones.Count);
+
+        Assert.Equal(ids.First(), deletedPhones.First().ID);
+    }
+
 }
 
 public class PhoneQueryData : TheoryData<List<string>>
 {
     public PhoneQueryData()
+    {
+        Add(
+            new List<string>()
+            {
+                "someguid"
+            }
+        );
+    }
+}
+
+public class PhoneCreateData : TheoryData<List<InputPhone>>
+{
+    public PhoneCreateData()
+    {
+        Add(
+            new List<InputPhone>()
+            {
+                new InputPhone(
+                    ID: null,
+                    Name: "Testing iPad",
+                    Description: "Testing iPad"
+                ),
+            }
+        );
+    }
+}
+
+public class PhoneUpdateData : TheoryData<List<InputPhone>>
+{
+    public PhoneUpdateData()
+    {
+        Add(
+            new List<InputPhone>()
+            {
+                new InputPhone(
+                    ID: "someguid",
+                    Name: "updated iPad nimi 4",
+                    Description: "Brand neeeeew iPad nimi 4"
+                ),
+            }
+        );
+    }
+}
+
+public class PhoneDeleteData : TheoryData<List<string>>
+{
+    public PhoneDeleteData()
     {
         Add(
             new List<string>()
